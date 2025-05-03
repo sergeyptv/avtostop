4609eeb30abc07086e729584f2dec4e1123f06c5
Sergey Petrov <personal.sergeypetrov@gmail.com>
2025-05-03 15:12:44 +0500

    maps are done.

--- easy level/map.go
+++ easy level/map.go
@@ -7,8 +7,11 @@ import (
 // 1
 // Что происходит?
 // Как сделать так, чтобы работало?
+//
+// При инициализации мапы через var m map[string]int - она будет равна nil, т.к. не создается hmap. Для того, чтобы
+// программа работала необходимо инициализировать мапу через m := make(map[string]int).
 func main() {
-	var m map[string]int
+	m := make(map[string]int, 7) // Указываем 7 потому, что заранее известна емкость мапы. Это позволит предотвратить эвакуацию данных.
 	for _, word := range []string{"hello", "world", "from", "the",
 		"best", "language", "in", "the", "world"} {
 		m[word]++
