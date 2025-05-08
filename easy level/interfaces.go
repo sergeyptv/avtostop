9812732f4afd2f66d00dccbd3c5a313ad394e702
Sergey Petrov <personal.sergeypetrov@gmail.com>
2025-05-08 13:35:06 +0500

    interfaces are done.

--- easy level/interfaces.go
+++ easy level/interfaces.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 // Что выведет код и почему?
+//
+// Выведет false, т.к. в функции A() происходит return с nil, а в функции B() - return с типом *impl, который
+// имплементирует интерфейс I. Таким образом, у нас будет a = nil типа и nil значения, а b = *impl типа и nil значения.
 type impl struct{}
 
 type I interface {
